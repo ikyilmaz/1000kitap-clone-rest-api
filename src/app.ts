d47115445aa9d9af0419786e2fae91c21b372a65
ismail <ismailkurbanyilmaz@gmail.com>
2020-05-12 00:02:11 +0300

    commit

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,7 @@ import { bookRouter } from './routes/book/book.route';
 import { bookLibraryRouter } from './routes/book-library.route';
 import { authorRouter } from './routes/author/author.route';
 import { currentUserRouter } from './routes/user/current-user.route';
+import moment from 'moment';
 
 const app = express();                                 // Express Engine
 
@@ -17,7 +18,7 @@ app.use(morgan('tiny'));                        // Logger
 
 // Routes
 app.use('/api/v1/users', userRouter);                   // USER
-app.use("/api/v1/current-user", currentUserRouter)      // CURRENT USER (Short-Hands for some requests)
+app.use('/api/v1/current-user', currentUserRouter);     // CURRENT USER (Includes Short-Hands for some requests)
 app.use('/api/v1/categories', categoryRouter);          // CATEGORY
 app.use('/api/v1/auth', authRouter);                    // AUTH
 app.use('/api/v1/book-libraries', bookLibraryRouter);   // BOOK LIBRARY (User's Libraries)
@@ -25,7 +26,13 @@ app.use('/api/v1/books', bookRouter);                   // BOOK
 app.use('/api/v1/authors', authorRouter);               // AUTHOR
 
 // Hello World Message
-app.all('/', ((req, res) => res.status(200).send('hello world')));
+app.all('/', ((req, res) => res.status(200).json({
+    hello: 'world',
+    date: new Date(Date.now())
+        .toLocaleDateString('tr',
+            { day: 'numeric', month: 'long', hour: 'numeric', minute: 'numeric', hour12: true, second: 'numeric' }
+        )
+})));
 
 // Error Controller
 app.use((err: Error | AppError, req: Request, res: Response, next: NextFunction) => {
