263781f5d7a99544ea33c97eb8ac4261a1b10da0
ismail <ismailkurbanyilmaz@gmail.com>
2020-05-11 17:08:34 +0300

    commit

--- src/controllers/book-library/book-library.controller.ts
+++ src/controllers/book-library/book-library.controller.ts
@@ -1,8 +1,13 @@
 import { BaseController } from '../base/base.controller';
 import { BookLibraryService } from './book-library.service';
+import { catchAsync } from '../../utils/catch-async';
 
 export class BookLibraryController extends BaseController {
     constructor(public bookLibraryService: BookLibraryService) {
         super(bookLibraryService.model);
     }
+
+    create = catchAsync(async (req, res, next) => {
+
+    })
 }
--- src/validators/body/book-library.validator.ts
+++ src/validators/body/book-library.validator.ts
@@ -1,14 +1,34 @@
-import { checkSchema } from "express-validator";
+import { checkSchema } from 'express-validator';
+import { isNotEmpty } from './lib/is-not-empty';
 
 const createOrUpdate = (isCreate: boolean) => checkSchema({
     name: {
-        isString: {
-
-        },
-
+        notEmpty: isCreate ? isNotEmpty('name') : undefined,
+        optional: !isCreate ? true : undefined,
+        isString: true,
+        isLength: {
+            errorMessage: 'field \'name\' must be between 1 and 64 characters',
+            options: { min: 1, max: 64 }
+        }
+    },
+    privacy: {
+        isString: true,
+        optional: true,
+        isIn: {
+            options: ['PRIVATE', 'PUBLIC'],
+            errorMessage: 'field \'privacy\' must be either \'PRIVATE\' or \'PUBLIC\''
+        }
+    },
+    description: {
+        isString: true,
+        optional: true,
+        isLength: {
+            errorMessage: 'field \'description\' must be between 1 and 255 characters',
+            options: { min: 1, max: 255 }
+        }
     }
-})
+});
 
 export const bookLibraryValidator = {
     createOrUpdate
-}
+};
